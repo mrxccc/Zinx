e114301c405b56df85c78c22a446d63359d0a612
mrxccc <mrxccc@qq.com>
2022-04-24 00:47:56 +0800

    初始代码

---
+++ ziface/iconnection.go
@@ -0,0 +1,23 @@
+package ziface
+
+import "net"
+
+//定义连接接口
+type IConnection interface {
+	//启动连接，让当前连接开始工作
+	Start()
+	//停止连接，结束当前连接状态M
+	Stop()
+	//从当前连接获取原始的socket TCPConn
+	GetTCPConnection() *net.TCPConn
+	//获取当前连接ID
+	GetConnID() uint32
+	//获取远程客户端地址信息
+	RemoteAddr() net.Addr
+	//直接将Message数据发送数据给远程的TCP客户端
+	SendMsg(msgId uint32, data []byte) error
+}
+
+//定义一个统一处理链接业务的接口
+// conn 当前连接；data 当前要处理的数据内容；len当前要处理数据的长度
+type HandFunc func(conn *net.TCPConn, data []byte, len int) error
