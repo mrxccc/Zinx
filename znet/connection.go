18f2174a250609af4da57bf61c256ef83283fcf1
mrxccc <mrxccc@qq.com>
2022-04-26 00:38:17 +0800

    v0.7:读写分离

--- znet/connection.go
+++ znet/connection.go
@@ -23,6 +23,9 @@ type Connection struct {
 
 	//告知该链接已经退出/停止的channel
 	ExitBuffChan chan bool
+
+	//无缓冲管道，用于读、写两个goroutine之间的消息通信
+	msgChan chan []byte
 }
 
 //创建连接的方法
@@ -33,6 +36,7 @@ func NewConntion(conn *net.TCPConn, connID uint32, msgHandler ziface.IMsgHandle)
 		isClosed:     false,
 		MsgHandler:   msgHandler,
 		ExitBuffChan: make(chan bool, 1),
+		msgChan:      make(chan []byte), //msgChan初始化
 	}
 	return c
 }
@@ -85,6 +89,29 @@ func (c *Connection) StartReader() {
 	}
 }
 
+/*
+	写消息Goroutine， 用户将数据发送给客户端
+*/
+func (c *Connection) StartWriter() {
+
+	fmt.Println("[Writer Goroutine is running]")
+	defer fmt.Println(c.RemoteAddr().String(), "[conn Writer exit!]")
+
+	for {
+		select {
+		case data := <-c.msgChan:
+			//有数据要写给客户端
+			if _, err := c.Conn.Write(data); err != nil {
+				fmt.Println("Send Data error:, ", err, " Conn Writer exit")
+				return
+			}
+		case <-c.ExitBuffChan:
+			//conn已经关闭
+			return
+		}
+	}
+}
+
 //直接将Message数据发送数据给远程的TCP客户端
 func (c *Connection) SendMsg(msgId uint32, data []byte) error {
 	if c.isClosed {
@@ -99,11 +126,7 @@ func (c *Connection) SendMsg(msgId uint32, data []byte) error {
 	}
 
 	//写回客户端
-	if _, err := c.Conn.Write(msg); err != nil {
-		fmt.Println("Write msg id ", msgId, " error ")
-		c.ExitBuffChan <- true
-		return errors.New("conn Write error")
-	}
+	c.msgChan <- msg //将之前直接回写给conn.Write的方法 改为 发送给Channel 供Writer读取
 
 	return nil
 }
@@ -111,8 +134,10 @@ func (c *Connection) SendMsg(msgId uint32, data []byte) error {
 //启动连接，让当前连接开始工作
 func (c *Connection) Start() {
 
-	//开启处理该链接读取到客户端数据之后的请求业务
+	//1 开启用户从客户端读取数据流程的Goroutine
 	go c.StartReader()
+	//2 开启用于写回客户端数据流程的Goroutine
+	go c.StartWriter()
 
 	for {
 		select {
@@ -141,6 +166,8 @@ func (c *Connection) Stop() {
 
 	//关闭该链接全部管道
 	close(c.ExitBuffChan)
+	close(c.msgChan)
+
 }
 
 //从当前连接获取原始的socket TCPConn
